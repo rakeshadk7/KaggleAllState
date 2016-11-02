2ad303284de86676c75ef190a7c0e628a5dd684a
Rakesh <rakesh.adk7@gmail.com>
2016-11-02 10:02:37 -0500

    Update Read File functions

--- AllstateSVM.py
+++ AllstateSVM.py
@@ -5,6 +5,21 @@ import numpy as np
 import csv
 import sys
 
+import pandas as pd
+
+########################################### Added by Rakesh Adhikesavan ##############################
+def readFile(path):
+    df = pd.read_csv(path) #returns a dataFrame, this can later be converted to a numpy array which is useful for sklearn's algorithms. 
+    return df
+
+        
+train = readFile("train.csv")
+test = readFile("test.csv")
+
+print train.head(10) #prints first 10 rows of the dataFrame
+
+########################################################################################################
+
 # First job is to read in the data from the training data that Kaggle provides. This
 # training data is in the form of a csv file. This CSV file should be in the same
 # directory as this program. 
